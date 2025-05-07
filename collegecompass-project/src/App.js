590a142029070ec16bc15ddae4909d5b1be35c4e
Tanishaa Singh <tanishaasingh@Tanishaas-MacBook-Air.local>
2025-05-07 12:25:38 -0700

    delete new file

--- collegecompass-project/src/App.js
+++ collegecompass-project/src/App.js
@@ -1,13 +1,21 @@
 // App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import CollegeCompassDash from './pages/UserDashboard';
-import './pages/UserDashboard'; 
+import MyNewPage from './pages/MyNewPage'; // <-- your new page
 
 function App() {
   return (
-    <div className="App">
-      <CollegeCompassDash />
-    </div>
+    <Router>
+      <Routes>
+        {/* Default Dashboard */}
+        <Route path="/" element={<CollegeCompassDash />} />
+
+        {/* New Page */}
+        <Route path="/my-new-page" element={<MyNewPage />} />
+      </Routes>
+    </Router>
   );
 }
 
