2883982599cc23419e82cf5462726e1d10bcc7bf
Tanishaa Singh <tanishaasingh@Tanishaas-MacBook-Air.local>
2025-05-09 10:38:45 -0700

    Edit Profile

---
+++ collegecompass-project/src/pages/EditProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EditProfile = () => {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>Edit Profile Page</h1>
+      <p></p>
+    </div>
+  );
+};
+
+export default EditProfile;
