b11c2f5b72baedff95e32e9a5b9e845e107b9209
Christina serrato <crisserr03@gmail.com>
2025-05-14 19:43:26 -0700

    Authentication

---
+++ collegecompass/src/App.js
@@ -0,0 +1,25 @@
+// import './App.css';
+// import Landing from './pages/landing';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Landing/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import './App.css';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
