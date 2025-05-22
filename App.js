6daacc43ff5a10152e3d20c230aa41deedbded13
ekerr003 <129991492+EllaKerrigan@users.noreply.github.com>
2025-05-22 15:50:40 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
-// App.js update - Add PlaceDetail route
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import PlaceDetail from './components/PlaceDetail';
-// Import your other pages/components
 
 function App() {
   return (
@@ -11,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/place/:placeId" element={<PlaceDetail />} />
-        {/* Your other routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
